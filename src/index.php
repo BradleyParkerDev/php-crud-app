3e372cf9d57e3a9e561be66a6f9f62f54b027242
BradleyParkerDev <bradleyscottparker@outlook.com>
2024-11-07 20:44:28 -0600

    Initial Commit

---
+++ src/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+</head>
+<body class="flex flex-col items-center justify-center min-h-screen bg-slate-300">
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <button class="text-white font-bold px-4 py-2 mt-4 bg-red-500 rounded-md hover:bg-red-600">
+    Press Me!!!!
+  </button>
+</body>
+</html>
